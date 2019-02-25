e37a441d5a3f2742daa90de71eb8787143de4bea
Jackson <jackson.christoffersen@joinhoney.com>
2019-02-25 15:07:07 -0800

    apollo boilerplate

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import client from './apollo';
+import Main from './view';
 
-const App = () => {
-  return <div>Hello React!</div>;
-};
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Main />
+  </ApolloProvider>
+);
+
 
 ReactDOM.render(<App />, document.getElementById('index'));
---
+++ src/view/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import { graphql, compose } from 'react-apollo';
+// import gql from 'graphql-tag';
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Sah, world.</h2>
+      </div>
+    );
+  }
+}
+
+export default Main;
